55529522f404a2df262729a4f9b871cbd1150037
@songrit0 <songrit208133@gmail.com>
2025-07-30 13:12:36 +0700

    fix Hours

--- events.py
+++ events.py
@@ -100,7 +100,7 @@ class BotEvents(commands.Cog):
                 hours = total_seconds // 3600
                 minutes = (total_seconds % 3600) // 60
                 seconds = total_seconds % 60
-                lines.append(f"{i:>2} | {hours} ชั่วโมง {minutes} นาที {seconds} วินาที | {name}")
+                lines.append(f"{i:>2} | {hours} Hours| {name}")
               
             msg = "\n".join(lines)
             import datetime
